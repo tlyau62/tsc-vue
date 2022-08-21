61b998627d914acfa29b34ae583b36dabba25ad3
Tsz Lam <tyautl@nexify.com.hk>
2022-08-21 11:05:33 +0800

    Update build config

--- packages/views/vite.config.js
+++ packages/views/vite.config.js
@@ -14,21 +14,11 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/main.ts"),
-      name: "MyLib",
-      // the proper extensions will be added
-      fileName: "my-lib",
+      fileName: "index",
+      formats: ["es"],
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-        },
-      },
     },
   },
 });
